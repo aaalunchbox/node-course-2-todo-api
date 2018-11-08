b34b5099d20e743cff7fdf6ad062a29955e17e12
aaalunchbox <lbox696@gmail.com>
2018-11-07 22:54:04 -0500

    Created functions for creating seed data for todos and users

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,22 +4,11 @@ const {ObjectID} = require('mongodb');
 
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
+const {todos, populateTodos, 
+       users, populateUsers} = require('./seed/seed');
 
-const todos = [{
-    _id: new ObjectID,
-    text: 'First test todo'
-},{
-    _id: new ObjectID,
-    text: 'Second test todo',
-    completed: true,
-    completedAt: 333
-}];
-
-beforeEach((done) => {
-    Todo.remove({}).then(() => {
-        return Todo.insertMany(todos);
-    }).then(() => done());
-});
+beforeEach(populateUsers);
+beforeEach(populateTodos);
 
 describe('POST /todos', () => {
     it('should create a new todo', (done) => {
@@ -181,3 +170,14 @@ describe('PATCH /todos/:id', () => {
             .end(done);
     });
 });
+
+// #TODO:  Tests for GET /Users/me
+// describe('GET /users/me', () => {
+//     it('should return user if authenticated', (done) => {
+
+//     });
+
+//     it('should return a 401 if not authenticated', (done) => {
+
+//     });
+// });
